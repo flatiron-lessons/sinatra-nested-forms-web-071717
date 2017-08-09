231e64eae20a72bf6ff1ef53306ee8dc1ad332bd
github username <github email address>
2017-08-09 13:55:15 -0400

    Done.

--- app.rb
+++ app.rb
@@ -3,7 +3,17 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    get '/new' do
+    	erb :"pirates/new"
+    end
+
+    post '/pirates' do
+    	erb :"pirates/show"
+    end
+
+    get '/' do
+    	"Welcome to the Nested Forms Lab! let's navigate to the '/new'"
+    end
 
   end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,21 @@
 class Ship
+
+	attr_reader :name, :weight, :height
+
+	SHIPS = []
+
+	def initialize(name, weight, height)
+		@name = name
+		@type = type
+		@booty = booty
+		SHIPS << self
+	end
+
+	def self.all
+		SHIPS
+	end
+
+	def self.clear
+		SHIPS.clear
+	end
 end
